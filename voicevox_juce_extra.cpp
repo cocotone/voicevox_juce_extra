2133357aed9bc69f463bc866a728a764364742ae
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-07-10 10:08:41 +0900

    Add source files.

---
+++ voicevox_juce_extra.cpp
@@ -0,0 +1,20 @@
+#include "voicevox_juce_extra.h"
+
+//==============================================================================
+#include "Engine/cocotone_TaskDetails.h"
+
+#include "Format/cocotone_FormatHelper.h"
+#include "Format/cocotone_AudioQueryConverter.h"
+#include "Format/cocotone_ScoreJsonConverter.h"
+
+#include "Static/cocotone_StaticMoraMapping.h"
+#include "Static/cocotone_StaticPhonemes.h"
+
+//==============================================================================
+#include "Engine/cocotone_VoicevoxEngine.cpp"
+
+#include "Format/cocotone_AudioQueryConverter.cpp"
+#include "Format/cocotone_ScoreJsonConverter.cpp"
+
+#include "Static/cocotone_StaticMoraMapping.cpp"
+#include "Static/cocotone_StaticPhonemes.cpp"
